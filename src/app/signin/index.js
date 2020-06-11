e746b2d65845f06c840ebd0dfc43fde9eabb2682
Vinícius Belló <bello.viniciusf@gmail.com>
2020-06-11 17:27:58 -0400

    add node_env on build script

--- src/app/signin/index.js
+++ src/app/signin/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
 import * as Yup from "yup";
-// import logo from "~/assets/logo.svg";
 import { signInRequest } from "../../store/modules/auth/actions";
 
 const schema = Yup.object().shape({
